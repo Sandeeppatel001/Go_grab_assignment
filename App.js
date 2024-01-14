5deee9ce9251022d2fd4c85f6ac31d58795aa14c
Sandeep Patel <sandeeppatelnwg@gmail.com>
2024-01-14 21:56:24 +0530

    Add to remote Github

---
+++ App.js
@@ -0,0 +1,31 @@
+import { ScrollView, StatusBar, View } from 'react-native';
+import styles from './App.components.style'
+import Header from './components/Header';
+import Task from './components/Task';
+import Form from './components/Form';
+import { useState, useEffect } from 'react';
+
+export default function App() {
+  const [taskList, setTaskList] = useState([])
+  const handleAddTask = (task) =>{
+    setTaskList([...taskList,task
+    ])
+  }
+  const handleRemoveTask = (number) =>{
+    let taskListTmp = [...taskList]
+    taskListTmp.splice(number,1)
+    setTaskList(taskListTmp);
+  }
+  return (
+    <View style = {styles.container}>
+      <StatusBar/>
+      <Header title = "To_do List"/>
+      <ScrollView style = {styles.scrollView}>
+        {taskList && taskList.map((task, index)=>{
+          return <Task key = {index + 1} content = {task} number = {index + 1} onDeleteTask = {handleRemoveTask}/>
+        })}
+      </ScrollView>
+      <Form onAddTask = {handleAddTask}/>
+    </View>
+  );
+}
---
+++ components/Header/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import color from "../../contains/color"
+
+const styles = StyleSheet.create({
+    title:{
+        color: '#1c8598',
+        fontSize: 40,
+        fontWeight: "bold",
+        marginTop: 40,
+        marginLeft: 20,
+    },
+})
+
+export default styles;
